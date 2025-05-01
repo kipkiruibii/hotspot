afc6a4e307dedf1990d36ce68f4214aeacb41048
kipkiruibii <infopack254@gmail.com>
2025-05-01 11:04:15 +0300

    wireguard mikrotik aws vps

--- hotspot_backend_portal/hotspot/hotspot_app/urls.py
+++ hotspot_backend_portal/hotspot/hotspot_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("initiate_pay",views.paymentSTK, name='initiate_payment'),
     path("payHeroCallback/", views.payHeroCallback, name="payHeroCallback"),
-    path("get_login_link/", views.get_login_link, name="get_login_link"),
+    path("get_login_link", views.get_login_link, name="get_login_link"),
 ]
